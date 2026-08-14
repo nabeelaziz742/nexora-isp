a47df689b4c3ac79fb3a1080c39afddfe958370f
Nabeel Aziz <155102918+nabeelaziz742@users.noreply.github.com>
2026-08-15 03:33:15 +0500

    chore: configure Django static files for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -73,6 +73,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.User"
 
